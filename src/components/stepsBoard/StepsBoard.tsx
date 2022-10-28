7f0d927d9050c4ea444f90d5a9318ebe60c6c6d6
Frans J Titulaer <fransjlt@gmail.com>
2022-10-28 13:58:57 +0200

    dying here

--- src/components/stepsBoard/StepsBoard.tsx
+++ src/components/stepsBoard/StepsBoard.tsx
@@ -1,47 +1,60 @@
 import StepsCard from "../stepsCard/stepsCard";
-import './stepsBoard.scss';
+import "./stepsBoard.scss";
 
 interface StepsBoardProps {
-    stepActiveIndex?: number,
-    small?: boolean
+  stepActiveIndex?: number;
+  small?: boolean;
 }
 
-function StepsBoard({stepActiveIndex, small = false}:StepsBoardProps) {
-    const steps = [
-        {
-            step: 'Step 1',
-            description: 'Choose your chef. We have verity of chefs that are expert in different cusines',
-            isActive: true,
-        },
-        {
-            step: 'Step 2',
-            description: 'Customize your meal. You can book our chefs for minimum 5 hr and maximum 8hr ',
-            isActive: true,
-        },
-        {
-            step: 'Step 3',
-            description: 'Checkout. Then sit back and relax',
-            isActive: true
-        }
-    ]
+function StepsBoard({ stepActiveIndex, small = false }: StepsBoardProps) {
+  const steps = [
+    {
+      step: "Step 1",
+      description:
+        "Choose your chef. We have verity of chefs that are expert in different cusines",
+      isActive: true,
+    },
+    {
+      step: "Step 2",
+      description:
+        "Customize your meal. You can book our chefs for minimum 5 hr and maximum 8hr ",
+      isActive: true,
+    },
+    {
+      step: "Step 3",
+      description: "Checkout. Then sit back and relax",
+      isActive: true,
+    },
+  ];
 
-    if (stepActiveIndex != undefined) {
-        steps.forEach((step, index) => {
-            if (index != stepActiveIndex) {
-                step.isActive = false;
-            }
-        })
-    }
+  if (stepActiveIndex !== undefined) {
+    steps.forEach((step, index) => {
+      if (index !== stepActiveIndex) {
+        step.isActive = false;
+      }
+    });
+  }
 
-    return (
-        <>
-            <section className={`steps-board-container ${small ? 'steps-board-small' : ''}`}>
-                {steps.map((el, index) => {
-                    return <StepsCard step={el.step} description={el.description} isActive={el.isActive} small={small} id={index} key={index} />
-                })}
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section
+        className={`steps-board-container ${small ? "steps-board-small" : ""}`}
+      >
+        {steps.map((el, index) => {
+          return (
+            <StepsCard
+              step={el.step}
+              description={el.description}
+              isActive={el.isActive}
+              small={small}
+              id={index}
+              key={index}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default StepsBoard;
