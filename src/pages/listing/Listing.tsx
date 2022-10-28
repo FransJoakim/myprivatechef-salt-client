a5284f862cf0b9f03a8b200d0413d7d29b99e5f8
Frans J Titulaer <fransjlt@gmail.com>
2022-10-28 12:56:20 +0200

    removed unused import

--- src/pages/listing/Listing.tsx
+++ src/pages/listing/Listing.tsx
@@ -1,22 +1,20 @@
-import Board from '../../components/board/Board';
-import { useEffect } from 'react';
-import { fetchChefs } from '../../features/chefs/chefSlice';
-import { useAppDispatch } from '../../features/hooks';
-import StepsBoard from '../../components/stepsBoard/StepsBoard';
-import HeroSub from '../../components/header/HeroSub';
-import './Listing.scss';
+import Board from "../../components/board/Board";
+import { useEffect } from "react";
+import { fetchChefs } from "../../features/chefs/chefSlice";
+import { useAppDispatch } from "../../features/hooks";
+import HeroSub from "../../components/header/HeroSub";
+import "./Listing.scss";
 
 function ListingPage() {
-
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchChefs());
-  }, [])
+  }, []);
 
   return (
     <>
-      <HeroSub title={'Choose your chef'} />
+      <HeroSub title={"Choose your chef"} />
       <div className="landingPage">
         <Board />
       </div>
